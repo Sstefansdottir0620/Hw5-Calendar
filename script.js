39345fbabe73e9517e06a18a655661704394e55e
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-02-11 20:17:22 -0600

    update

--- script.js
+++ script.js
@@ -1,11 +1,79 @@
 
 
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function() {
+        var value = $(this).siblings("textarea").val();
+        var time = $(this).siblings().attr("id");
+
+        localStorage.setItem(time, value);
+        
+    })
+
+    $("#hour-8 textarea").val(localStorage.getItem("hour-8"));
+    $("#hour-9 textarea").val(localStorage.getItem("hour-9"));
+    $("#hour-10 textarea").val(localStorage.getItem("hour-10"));
+    $("#hour-11 textarea").val(localStorage.getItem("hour-11"));
+    $("#hour-12 textarea").val(localStorage.getItem("hour-12"));
+    $("#hour-13 textarea").val(localStorage.getItem("hour-13"));
+    $("#hour-14 textarea").val(localStorage.getItem("hour-14"));
+    $("#hour-15 textarea").val(localStorage.getItem("hour-15"));
+    $("#hour-16 textarea").val(localStorage.getItem("hour-16"));
+    $("#hour-17 textarea").val(localStorage.getItem("hour-17"));
+    $("#hour-18 textarea").val(localStorage.getItem("hour-18"));
+
 //when I open the Work Day Scheduler the currentTime will appear
 
-var currentDay = document.getElementById("currentDay")
 
-var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").append(currentTime)
-console.log(currentTime)
 
-//When I add a task to the scheduler it should append and remain there when we push the "saveBtn".
+
+
+function hourUpdate(){
+    var currentHour = (moment().hours());
+    console.log($(".time-block"));
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        console.log("test")
+        
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            
+        }
+
+        else if (blockHour === currentHour) {
+            $(this).addClass("past");
+            $(this).addClass("present");
+        }
+
+        else{
+            $(this).addClass("past");
+            $(this).addClass("present");
+            $(this).addClass("future");
+        }
+        
+    });
+
+}
+
+hourUpdate();
+
+
+var interval = setInterval(hourUpdate, 15000);
+
+$("#hour-8 .textarea").val(localStorage.getItem("hour-8"));
+$("#hour-9 .textarea").val(localStorage.getItem("hour-9"));
+$("#hour-10 .textarea").val(localStorage.getItem("hour-10"));
+$("#hour-11 .textarea").val(localStorage.getItem("hour-11"));
+$("#hour-12 .textarea").val(localStorage.getItem("hour-12"));
+$("#hour-13 .textarea").val(localStorage.getItem("hour-13"));
+$("#hour-14 .textarea").val(localStorage.getItem("hour-14"));
+$("#hour-15 .textarea").val(localStorage.getItem("hour-15"));
+$("#hour-16 .textarea").val(localStorage.getItem("hour-16"));
+$("#hour-17 .textarea").val(localStorage.getItem("hour-17"));
+$("#hour-18 .textarea").val(localStorage.getItem("hour-18"));
+
+var currentHour = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+$("#currentDay").append(currentHour)
+
+});
